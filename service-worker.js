799d60e3fe89c32fcb33f667d10a52aa6d6dd21b
Yiqin Zhao <yiqinzhao@outlook.com>
2019-07-18 22:23:18 +0800

    rebuilding site on Thu Jul 18 22:23:18 CST 2019, commit  and job

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08646852c71cdd6c4581daf7d43abd09.js"
+  "/galaxy/precache-manifest.5737d5863d09dcd32caf134e6532d108.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/galaxy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
